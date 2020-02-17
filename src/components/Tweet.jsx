52239e852bbc366ea638d854c926c634886f777c
Student <example@example.com>
2020-02-17 14:58:54 -0800

    add like functionality to our own tweets

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FaFish } from 'react-icons/fa';
 
-function Tweet(props){
+class Tweet extends React.Component {
 
-  var tweetBox = {
-    textAlign: 'center',
-    border: '2px solid black',
-    borderRadius: '5px',
-    marginBottom: '20px',
-    width: '300px'
+  constructor(props) {
+    super(props);
+    this.state = {
+      fishCount: 0
+    };
+    this.increaseFishCount = this.increaseFishCount.bind(this);
   }
+
+  increaseFishCount(event) {
+    let newFishCount = this.state.fishCount + 1
+    this.setState({fishCount: newFishCount});
+  }
+
+
+render() {
   return (
-    <div style={tweetBox}>
-      <h3> {props.title}</h3>
-      <p>{props.body}</p>
+    <div>
+    <h3> {this.props.title}</h3>
+    <p>{this.props.body}</p>
+    <a><FaFish onClick={this.increaseFishCount}/></a><span>{this.state.fishCount}</span>
     </div>
   );
+}
 }
 
 Tweet.propTypes = {
@@ -24,3 +35,12 @@ Tweet.propTypes = {
 };
 
 export default Tweet;
+
+//
+// var tweetBox = {
+//   textAlign: 'center',
+//   border: '2px solid black',
+//   borderRadius: '5px',
+//   marginBottom: '20px',
+//   width: '300px'
+// }
